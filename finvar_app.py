507be8bf4ee2361ed79b5b82e92d27ac8f4a7f84
AtulAravindDas <161445393+AtulAravindDas@users.noreply.github.com>
2025-04-21 21:50:16 -0400

    Update finvar_app.py

--- finvar_app.py
+++ finvar_app.py
@@ -13,7 +13,10 @@ if user_input:
     st.header("🏢 Company Overview")
     company_name = info.get('longName', 'N/A')
     st.markdown(f"<h2 style='font-size:32px; color:#FFFFFF;'>🏢 Company Name: {company_name}</h2>", unsafe_allow_html=True)
-    st.write(f"**Description:** {info.get('longBusinessSummary', 'N/A')}")
+    description = info.get('longBusinessSummary', 'N/A')
+
+    st.markdown(f"""<div style="background-color:#f0f2f6; padding:15px 20px; border-radius:10px; line-height:1.6; color:#000000; font-size:16px; max-width:800px;">
+    <strong>Description:</strong><br>{description}</div>""", unsafe_allow_html=True)
 
     current_price = info.get("currentPrice", "N/A")
     prev_close = info.get("previousClose", "N/A")
